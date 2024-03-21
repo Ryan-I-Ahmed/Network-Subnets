0a8ba2bde472e05aae66358d1ff2c3baa7568200
Ryan-I-Ahmed <159076403+Ryan-I-Ahmed@users.noreply.github.com>
2024-03-21 22:54:30 +0530

    Add files via upload

---
+++ subnet.py
@@ -0,0 +1,75 @@
+#enter your unique code
+uniquecode = 11551
+
+#problem 1
+p1Network = '57.30.94.128'
+p1Broadcast = '57.30.94.255'
+p1First = '57.30.94.129'
+p1Last =  '57.30.94.254'
+
+#problem 2
+p2Network = '224.15.56.240'
+p2Broadcast = '224.15.56.255'
+p2First = '224.15.56.241'
+p2Last = '224.15.56.254'
+
+#problem 3
+p3Network = '135.0.18.0'
+p3Broadcast = '135.0.18.127'
+p3First = '135.0.18.1'
+p3Last = '135.0.18.126'
+
+#problem 4
+p4Network = '46.241.236.64'
+p4Broadcast = '46.241.236.71'
+p4First = '46.241.236.65'
+p4Last = '46.241.236.70'
+
+#problem 5
+p5Network = '213.226.198.64'
+p5Broadcast = '213.226.198.127'
+p5First = '213.226.198.65'
+p5Last = '213.226.198.126'
+
+#problem 6
+p6Network = '124.212.159.0'
+p6Broadcast = '124.212.159.255'
+p6First = '124.212.159.1'
+p6Last =  '124.212.159.254'
+
+#problem 7
+p7Network = '35.197.121.192'
+p7Broadcast = '35.197.121.223'
+p7First = '35.197.121.193'
+p7Last = '35.197.121.222'
+
+#problem 8
+p8Network = '202.182.83.128'
+p8Broadcast = '202.182.83.255'
+p8First = '202.182.83.129'
+p8Last = '202.182.83.254'
+
+#problem 9
+p9Network = '113.167.45.16'
+p9Broadcast = '113.167.45.31'
+p9First = '113.167.45.17'
+p9Last = '113.167.45.30'
+
+#problem 10
+p10Network = '24.152.6.64'
+p10Broadcast = '24.152.6.127'
+p10First = '24.152.6.65'
+p10Last = '24.152.6.126'
+
+
+"""
+example:
+
+uniquecode = 12345
+
+problem 1
+p1Network = '199.204.14.128'
+p1Broadcast = '199.204.14.255'
+p1First = '199.204.14.129'
+p1Last =  '199.204.14.254'
+"""
